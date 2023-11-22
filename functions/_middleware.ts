84cab6b90a7da484e12a782154d423d5d722e2c9
Julio Rodriguez <juliorodriguez@ncl.com>
2023-11-22 15:12:45 -0500

    Worker for email

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,15 +1,19 @@
-import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+import {PagesFunction} from "@cloudflare/workers-types";
 
-export const onRequest = mailchannelsPlugin({
+export const onRequest: PagesFunction = mailChannelsPlugin({
     personalizations: [
         {
             to: [{ name: "Web contact", email: "julioarhernandez@gmail.com" }],
         },
     ],
-    from: { name: "Enquiry", email: "no-reply@example.com" },
-    respondWith: () =>
-        new Response(null, {
-            status: 302,
-            headers: { Location: "/thank-you" },
-        }),
+    from: {
+        name: "ACME Support",
+        email: "support@example.com",
+    },
+    respondWith: () => {
+        return new Response(
+            `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+        );
+    },
 });
